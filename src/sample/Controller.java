2cece6b5fa419b042d512ff89c456fc4d3462d0e
Nicolasx08 <nic.pageau@hotmail.com>
2018-11-15 09:30:15 -0500

    Disable les button

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -13,13 +13,13 @@ public class Controller {
     private Button but;
     public static Matrice matrice = new Matrice();
     public static ArrayList<Element> element = new ArrayList<>();
-    public static int temp;
     public static Spinner spinnerL= new Spinner(1,4,3);
     public static Spinner spinnerC= new Spinner(1,4,3);
     public static Label labelL = new Label("Entrez le nombre de lignes de votre matrice     ");
     public static Label labelC = new Label("Entrez le nombre de colonnes de votre matrice");
     @FXML
     public void nouvelleMatrice(){
+        ButtonType buttonDialog1 = new ButtonType("ok", ButtonBar.ButtonData.OK_DONE);
         AtomicInteger varTemp=new AtomicInteger();
         varTemp.set(0);
         HBox hb = new HBox(labelL,spinnerL);
@@ -40,9 +40,11 @@ public class Controller {
         for (int i=0;i<var;i++){
             tabBut[i]=new Button();
             tabBut[i].setText("["+i+"]");
+            tabBut[i].setMaxSize(50,50);
+            tabBut[i].setMinSize(50,50);
         }
-        int constante=0;
         VBox vbTemp= new VBox();
+        int constante=0;
         for (int i=0;i<matrice.getTailleL();i++){
             HBox hbTemp = new HBox();
             hbTemp.setSpacing(7);
@@ -55,46 +57,48 @@ public class Controller {
         }
         Dialog dialog1 = new Dialog();
         dialog1.getDialogPane().setContent(vbTemp);
-        for (int i=0;i<var;i++){
-            if (!tabBut[i].getText().equals("["+i+"]")){
-                varTemp.set(varTemp.get()+1);
-            }
-        }
-        if (varTemp.get()==var){
-            dialog1.getDialogPane().getButtonTypes().add( new ButtonType("OK", ButtonBar.ButtonData.OK_DONE));
-        }
-
         dialog1.show();
 
         Label question = new Label("Entrez la valeur ");
-        TextField tf = new TextField();
+        TextField tf = new TextField("1");
         HBox labTf = new HBox(question,tf);
         labTf.setSpacing(7);
+        int l=0;
+        for (int i=0;i<var;i++){
+            tabBut[i].setDisable(true);
+            if (tabBut[l]==tabBut[i] && tabBut[l].getText().equals("["+l+"]") || l==0){
+                tabBut[i].setDisable(false);
+                l++;
+            }
+        }
         for (int i=0;i<var;i++){
             final int temp=i;
             tabBut[i].setOnAction((event)->{
+                Element elem= new Element();
                Dialog dialog2 = new Dialog();
                dialog2.getDialogPane().setContent(labTf);
-               if (!tf.equals("")){
-                   dialog2.getDialogPane().getButtonTypes().add( new ButtonType("OK", ButtonBar.ButtonData.OK_DONE));
-               }
+               dialog2.getDialogPane().getButtonTypes().add( new ButtonType("OK", ButtonBar.ButtonData.OK_DONE));
                dialog2.showAndWait();
+               elem.setValeur(Integer.parseInt(tf.getText()));
+               element.add(elem);
+               matrice.setElement(element);
                tabBut[temp].setText(tf.getText());
-               tf.setText("");
+               tf.setText("1");
                varTemp.set(varTemp.get()+1);
+               if (varTemp.get()==var){
+                  dialog1.getDialogPane().getButtonTypes().add(buttonDialog1);
+                  dialog1.setWidth(500);
+                  dialog1.setHeight(500);
 
+               }
             });
         }
-    }
-    @FXML
-    public void valeurMat(){
-
-
 
     }
     @FXML
-    public void addElement(){
-
-
+    public void supprimerMatrice(){
+        for (int j=0; j<element.size();j++){
+            System.out.println(matrice.getElement().get(j).getValeur());
+        }
     }
 }
