f07bdcd860d23b9650459eb088de1b6c0284aec4
Nicolasx08 <nic.pageau@hotmail.com>
2018-12-08 11:42:26 -0500

    manque tenso et CSV

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,19 +8,22 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application {
-
+   private static Parent root;
+   public static Stage stage1;
     @Override
     public void start(Stage stage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        stage1=stage;
+        root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         stage.setTitle("Matrice");
         stage.setMaximized(true);
         stage.setResizable(false);
         stage.setScene(new Scene(root));
         stage.show();
     }
-
-
     public static void main(String[] args) {
         launch(args);
+    }
+    public static Parent returnRoot(){
+        return root;
     }
 }
